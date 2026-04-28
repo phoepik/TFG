523beab38eb67f007f6aaf3fda3ebaf8626ad23b
sorolleta <sorollayeray@gmail.com>
2026-04-28 18:10:57 +0200

    fix: corregir URL del servidor http:/ -> http://

--- bocetoCalendario1_funcional/app/src/main/java/com/example/bocetocalendario1/network/RetrofitClient.kt
+++ bocetoCalendario1_funcional/app/src/main/java/com/example/bocetocalendario1/network/RetrofitClient.kt
@@ -7,7 +7,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
 object RetrofitClient {
-    private const val BASE_URL = "http:/18.215.56.24:8080/"
+    private const val BASE_URL = "http://18.215.56.24:8080/"
 
     private val loggingInterceptor = HttpLoggingInterceptor().apply {
         level = HttpLoggingInterceptor.Level.BODY
